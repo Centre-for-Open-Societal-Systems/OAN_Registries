a7aefa0a970df0f749b896b4959dd028bad2c7cb
eldho51248 <76879802+eldho51248@users.noreply.github.com>
2026-08-19 11:56:48 +0530

    Add files via upload

---
+++ g2p_livestock_registry/models/__init__.py
@@ -0,0 +1,7 @@
+from . import livestock_registry
+from . import livestock_event
+from . import livestock_vaccine_schedule
+from . import livestock_audit
+from . import livestock_import_batch
+from . import live_stock_registry_line
+from . import live_stock_dashboard
